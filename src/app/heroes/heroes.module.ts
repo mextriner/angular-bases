3d461c3ac73d3f8bc54ac24120ad2c94dcd5a940
mextriner <mextrinerx1998@gmail.com>
2023-05-08 11:16:37 +0200

    Fin sección 4

---
+++ src/app/heroes/heroes.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { HeroComponent } from "./components/heroes/hero/hero.component";
+import { ListComponent } from "./components/heroes/list/list.component";
+
+
+@NgModule({
+  declarations:[
+    HeroComponent,
+    ListComponent
+  ],
+  exports:[
+    HeroComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+
+export class HeroesModule{
+
+}
